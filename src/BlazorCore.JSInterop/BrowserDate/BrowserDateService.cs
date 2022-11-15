c58ceab66aacda65a884649deb57e3bdc3233038
Han Chee <hanchee@codecultivation.com>
2022-11-15 22:03:21 +0000

    Add browser date service.

---
+++ src/BlazorCore.JSInterop/BrowserDate/BrowserDateService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorCore.JSInterop.BrowserDate;
+
+public class BrowserDateService : IBrowserDateService
+{
+	private readonly IJSRuntime _jSRuntime;
+
+	public BrowserDateService(IJSRuntime jSRuntime)
+	{
+		_jSRuntime = jSRuntime;
+	}
+
+	public async Task<DateTime> GetBrowserDateTimeAsync()
+	{
+		return await _jSRuntime.InvokeAsync<DateTime>("eval", "(new Date()).toJSON();");
+	}
+}
---
+++ src/BlazorCore.JSInterop/BrowserDate/IBrowserDateService.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorCore.JSInterop.BrowserDate;
+
+public interface IBrowserDateService
+{
+	/// <summary>
+	/// Returns Date and time from browser client machine.
+	/// </summary>
+	/// <returns>Async Task</returns>
+	Task<DateTime> GetBrowserDateTimeAsync();
+}
